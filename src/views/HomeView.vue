a17ebea0d513823e84e50f91557ad8b463531538
eduado <eduardo.hor@outlook.com>
2023-03-21 10:08:28 -0300

    :lipstick: feat: adding listing of all contacts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,4 @@
-<script setup>
-import Header from '../components/Header.vue'
-import Form from '../components/Form.vue'
 
-
-</script>
 <template>
 	<main>
 		<div class="container py-4">
@@ -20,7 +15,7 @@ import Form from '../components/Form.vue'
 			<Form idName="adicionarContato"></Form>
 
 			<div class="row align-items-md-stretch">
-				<div class="col-md-6">
+				<div v-for="contact of contacts" :key="contact.id" class="col-md-6">
 					<div class="h-80 p-5 bg-light border rounded-3">
 						<div>
 							<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
@@ -28,41 +23,12 @@ import Form from '../components/Form.vue'
 								<path
 									d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
 							</svg>
-							<h2>Nome</h2>
+							<h2>{{ contact.name }}</h2>
 						</div>
-						<p>(99)912332112</p>
+						<p>{{ contact.telephone }}</p>
 						<button class="btn btn-outline-secondary" type="button">Detalhes</button>
 					</div>
 				</div>
-				<div class="col-md-6">
-					<div class="h-80 p-5 bg-light border rounded-3">
-						<div>
-							<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-								class="bi bi-person-lines-fill" viewBox="0 0 16 16">
-								<path
-									d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
-							</svg>
-							<h2>Nome</h2>
-						</div>
-						<p>(99)912332112</p>
-						<button class="btn btn-outline-secondary" type="button">Detalhes</button>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="h-80 p-5 bg-light border rounded-3">
-						<div>
-							<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-								class="bi bi-person-lines-fill" viewBox="0 0 16 16">
-								<path
-									d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z" />
-							</svg>
-							<h2>Nome</h2>
-						</div>
-						<p>(99)912332112</p>
-						<button class="btn btn-outline-secondary" type="button">Detalhes</button>
-					</div>
-				</div>
-
 			</div>
 
 			<footer class="pt-3 mt-4 text-muted border-top">
@@ -71,3 +37,34 @@ import Form from '../components/Form.vue'
 		</div>
 	</main>
 </template>
+
+<script>
+import Header from '../components/Header.vue'
+import Form from '../components/Form.vue'
+import { axiosInstance } from '../services/http'
+
+export default {
+	components:{
+		Header,
+		Form
+	},
+  data() {
+    return {
+      contacts: [],
+      url: `${import.meta.env.VITE_API_URL_IMG}`
+    }
+  },
+
+  async created() {
+    try {
+      const { data } = await axiosInstance.get('/contacts');
+      this.contacts = data
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+}
+
+
+</script>
