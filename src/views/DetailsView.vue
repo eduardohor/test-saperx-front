62edf48a65cdebb7f4d3d51ce23d981b6a429938
eduado <eduardo.hor@outlook.com>
2023-03-21 11:15:00 -0300

    :lipstick: feat: adding contact deletion

--- src/views/DetailsView.vue
+++ src/views/DetailsView.vue
@@ -15,12 +15,34 @@
 					<p class="col"><span class="fw-bold">Data do cadastro:</span> {{ contact.created_at }}</p>
 				</div>
 				<div class="col mt-5">
-					<router-link :to="{ name: 'edit', params: { id: contact.id } }" class="btn btn-warning" 						role="button">Editar</router-link>
-					<button class="btn btn-danger m-2">Excluir</button>
-					
+					<router-link :to="{ name: 'edit', params: { id: contact.id } }" class="btn btn-warning"
+						role="button">Editar</router-link>
+					<button data-bs-toggle="modal" data-bs-target="#deleteContact" class="btn btn-danger m-1"
+						role="button">Excluir</button>
 				</div>
 			</div>
-			
+
+			 <!-- Modal -->
+			 <div class="modal fade" id="deleteContact" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="exampleModalLabel">Excluir </h5>
+							<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+						</div>
+						<div class="modal-body">
+							Deseja realmente excluir o contato {{ contact.name }} ? 
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+							<form @submit.prevent="remove">
+								<button type="submit" class="btn btn-danger" data-bs-dismiss="modal">Excluir</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+
 
 		</div>
 	</div>
@@ -36,21 +58,32 @@ export default {
 	},
 
 	data() {
-    return {
-      contact: [],
-      id:this.$route.params.id,
-    }
-  },
+		return {
+			contact: [],
+			id: this.$route.params.id,
+		}
+	},
 
-  async created() {
-    try {
-      const { data } = await axiosInstance.get(`/contacts/${this.id}`);
-      this.contact = data.contact
+	async created() {
+		try {
+			const { data } = await axiosInstance.get(`/contacts/${this.id}`);
+			this.contact = data.contact
 			console.log(data)
-    } catch (error) {
-      console.log(error)
+		} catch (error) {
+			console.log(error)
+		}
+	},
+
+	methods:{
+    async remove(){
+      try {
+          const { data } = await axiosInstance.delete(`/contacts/${this.id}`)
+          this.$router.push({ name: 'home' });
+      } catch (error) {
+        console.log(error)
+      }
     }
-  },
+  }
 }
 
 </script>
